0472e883b08d7c870f98d5d73ce2c885d4be8b00
Iamneo <techadmin@iamneo.ai>
2023-08-08 05:16:36 +0000

    Backup Commit

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -1,20 +1,16 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
-import { CurrencyconvPipe } from './currencyconv.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CurrencyconvPipe
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angularapp/src/app/currencyconv.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CurrencyconvPipe } from './currencyconv.pipe';
-
-describe('CurrencyconvPipe', () => {
-  it('create an instance', () => {
-    const pipe = new CurrencyconvPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
